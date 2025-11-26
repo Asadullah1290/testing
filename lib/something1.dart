752fe56c90e64e95ad1772556544340f1e7b43f3
Asadullah1290 <masculineman786@gmail.com>
2025-11-26 19:21:15 +0500

    changed

--- lib/something1.dart
+++ lib/something1.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      // hello woerl
       appBar: AppBar(
         title: const Text("this is a house"),
         backgroundColor: Colors.amber,
