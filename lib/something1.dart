c7df094e4009ebb16c606c430947f0f32aae7b39
Asadullah1290 <masculineman786@gmail.com>
2025-11-26 19:19:24 +0500

    commit

---
+++ lib/something1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("this is a house"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    ),
+  ));
+}
